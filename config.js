89a511bdf72d372c69e56eec0c9bd550ed04473d
ALKASER7008 <166563424+ALKASER7008@users.noreply.github.com>
2024-05-18 00:51:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,24 +3,24 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
+global.owner = process.env.OWNER_NUMBER.split("+967718661304")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://KING-MDBOT:KING-MDBOT@cluster0.ltjjfkx.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = ''
 global.github = 'h'
 global.location = 'Sultanpur IN'
 global.gurl = 'لا يوجد' // add your username
-global.sudo = process.env.SUDO || '994400668447'
-global.devs = '994400668447';
-global.website = 'https://chat.whatsapp.com/H7lNIia7rsaDcYSNWrtTGI' //wa.me/+25000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/9ff660f01530e28515ccc.jpg'
+global.sudo = process.env.SUDO || '967718661304'
+global.devs = '967718661304';
+global.website = '//wa.me/+967718661304'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/8b4c46a8e8e62c5834b7f.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? '*𝑨𝑳𝑲𝑨𝑺𝑬𝑹*' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? '𝑨𝑳𝑲𝑨𝑺𝑬𝑹↝𝑩𝑶𝑻' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? '*هيسوكا و القط طحنون*' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'القط طحنون' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_INFO.split(";")[0] === undefined ? '*𝑨𝑳𝑲𝑨𝑺𝑬𝑹↝𝑩𝑶𝑻*' : process.env.PACK_INFO.split(";")[0],
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? '*القط طحنون*' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '𝑨𝑳𝑲𝑨𝑺𝑬𝑹' : process.env.PACK_INFO.split(";")[1],
+  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'القط طحنون' : process.env.PACK_INFO.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
